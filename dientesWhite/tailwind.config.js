3774c01def8194fc3ecc5d8453a6ba0e1338bbf6
DiegoNar-1970 <diegonaranjo303@gmail.com>
2024-11-08 23:14:58 -0500

    styles

--- dientesWhite/tailwind.config.js
+++ dientesWhite/tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        scaleUpDown: {
+          '0%, 100%': { transform: 'scale(1)' },
+          '50%': { transform: 'scale(1.5)' }, 
+        },
+      },
+      animation: {
+        scalePulse: 'scaleUpDown 2s infinite ease-in-out', 
+      },
+    },
   },
   plugins: [],
 }
