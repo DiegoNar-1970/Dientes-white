b1a2040ed8b20431964acc678e0495702d734769
DiegoNar-1970 <diegonaranjo303@gmail.com>
2024-11-09 22:02:32 -0500

    no avance mucho hoy la verdad

--- dientesWhite/tailwind.config.js
+++ dientesWhite/tailwind.config.js
@@ -10,11 +10,17 @@ module.exports = {
           '0%, 100%': { transform: 'scale(1)' },
           '50%': { transform: 'scale(1.5)' }, 
         },
+        bounce: { '0%, 100%': { transform: 'translateY(0)' }, 
+                  '50%': { transform: 'translateY(-10px)' },
+              }
       },
       animation: {
         scalePulse: 'scaleUpDown 2s infinite ease-in-out', 
+         bounce: 'bounce 1.7s ease-in-out infinite',
       },
+
     },
   },
   plugins: [],
 }
+ 
